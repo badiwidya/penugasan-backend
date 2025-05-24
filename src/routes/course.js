14b58e9286f864b313895cb0ae5fa1a9a7f22d18
mixinit <adiwidya.berton13@gmail.com>
2025-05-24 20:29:43 +0700

    Tambah handler di endpoint /courses

--- src/routes/course.js
+++ src/routes/course.js
@@ -1,8 +1,9 @@
 import express from "express";
+import CourseController from "../controllers/CourseController.js";
 
 const router = express.Router();
 
-router.get("/courses");
+router.get("/courses", CourseController.getAllCourses);
 router.get("/courses/:courseId");
 router.get("/courses/:courseId/students");
 router.get("/courses/:courseId/teachers");
