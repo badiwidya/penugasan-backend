83f1c16cd1565b5a000d06f3ebfe8b43d6babc57
mixinit <adiwidya.berton13@gmail.com>
2025-05-24 22:30:02 +0700

    Set kredensial di middleware, dan return kalo ga punya token

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,9 +1,12 @@
+import { oauth2Client } from "../config/googleConfig.js";
+
 export default function (req, res, next) {
     if (!req.session.tokens) {
-        res.status(401).json({
+        return res.status(401).json({
             status: "failed",
             error: "Unauthorized",
         });
     }
+    oauth2Client.setCredentials(req.session.tokens);
     next();
 }
