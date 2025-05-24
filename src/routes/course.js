8f2dc9f8c3a90313f474f3f91cc1b603f70d5018
mixinit <adiwidya.berton13@gmail.com>
2025-05-24 20:53:26 +0700

    Buat dummy handler di routes course

--- src/routes/course.js
+++ src/routes/course.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get("/", CourseController.getAllCourses);
 router.get("/:courseId", CourseController.getCourse);
-router.get("/:courseId/students");
-router.get("/:courseId/teachers");
-router.get("/:courseId/assignments");
+router.get("/:courseId/students", (req, res) => {});
+router.get("/:courseId/teachers", (req, res) => {});
+router.get("/:courseId/assignments", (req, res) => {});
 
 export default router;
