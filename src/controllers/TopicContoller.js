9cf222a72fbae49e91e56a423bbe859916027bd6
mixinit <adiwidya.berton13@gmail.com>
2025-05-27 16:41:53 +0700

    Buat handler untuk createbatchtopics

--- src/controllers/TopicContoller.js
+++ src/controllers/TopicContoller.js
@@ -5,6 +5,7 @@ class TopicController {
         this.service = TopicService;
 
         this.getAllTopics = this.getAllTopics.bind(this);
+        this.createBatchTopics = this.createBatchTopics.bind(this);
     }
 
     async getAllTopics(req, res, next) {
@@ -21,7 +22,35 @@ class TopicController {
         }
     }
 
-    
+    async createBatchTopics(req, res, next) {
+        try {
+            const { topics } = req.body;
+
+            if (!topics || !Array.isArray(topics) || topics.length === 0) {
+                const error = new Error("Array topics dibutuhkan");
+                error.statusCode = 400;
+                throw error;
+            }
+
+            const responses = await this.service.createBatchTopics(topics);
+
+            if (responses.some((response) => response.success === false)) {
+                return res.json({
+                    status: true,
+                    message: "Topik berhasil dibuat, tetapi ada beberapa yang gagal",
+                    data: responses,
+                });
+            }
+
+            res.json({
+                status: true,
+                message: "Topik berhasil dibuat",
+                data: responses,
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 export default new TopicController();
