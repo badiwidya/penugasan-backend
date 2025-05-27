d908a09e4eb0868674c854069c57ef3bfa505156
mixinit <adiwidya.berton13@gmail.com>
2025-05-28 06:17:57 +0700

    Hapus yang ga perlu di assignment route

--- src/routes/assignment.js
+++ src/routes/assignment.js
@@ -5,9 +5,5 @@ const router = express.Router();
 
 router.get("/", AssignmentController.getAllAssignments);
 router.post("/batch", AssignmentController.createBatchAssignments);
-router.get("/", (req, res) => {});
-router.get("/", (req, res) => {});
-router.get("/", (req, res) => {});
-router.get("/", (req, res) => {});
 
 export default router;
